52d9a49088a959f294447587beb6d356b7b7dcdd
anubra266 <anubra266@gmail.com>
2021-04-28 19:50:17 +0100

    🍱 Remove main mappings

--- resources/scripts/main.tsx
+++ resources/scripts/main.tsx
@@ -1,14 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Main entry point
-|--------------------------------------------------------------------------
-| Files in the "resources/scripts" directory are considered entrypoints
-| by default. 
-| 
-| -> https://vitejs.dev
-| -> https://github.com/innocenzi/laravel-vite
-*/
-
 import { render } from "react-dom";
 import App from "@/views/App";
 
